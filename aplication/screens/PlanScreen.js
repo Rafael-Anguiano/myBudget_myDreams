70de195a42dc7f26aed09a7e84d8009078185a16
Rafael Anguiano <rj.anguiano.23@gmail.com>
2021-03-14 03:12:43 -0600

    Calculating the number of fortnights at the fist month

--- aplication/screens/PlanScreen.js
+++ aplication/screens/PlanScreen.js
@@ -2,48 +2,132 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Text, Button, Item, Input, Label } from 'native-base'
-import DateTimePickerModal from "react-native-modal-datetime-picker";
-import moment from 'moment'
 
 export default class SavingPlan extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            reason: undefined,
+            amount: undefined,
+            choosenDate: undefined,
+            fortnights: 0,
+            //StrtDate
             strtDay: undefined,
             strtMonth: undefined,
             strtYear: undefined,
-
-            reason: undefined,
-            amount: undefined,
-
-            isVisible: false,
-            choosenDate: undefined,
+            //DueDate
+            dueDay: undefined,
+            dueMonth: undefined,
+            dueYear: undefined,
+            calculated: false
         };
     }
 
-    componentDidMount(){
-        this.setState({
+    async componentDidMount(){
+        await this.setState({
             strtDay: this.props.route.params.strtDay,
             strtMonth: this.props.route.params.strtMonth,
             strtYear: this.props.route.params.strtYear,
             reason: this.props.route.params.reason,
             amount: this.props.route.params.amount,
-            choosenDate: this.props.route.params.choosenDate,
+            choosenDate: this.props.route.params.choosenDate
         })
+        this.setPlan()
     }
 
+    setPlan(){
+        const strtDay = this.state.strtDay
+        const strtMonth = this.state.strtMonth
+        const strtYear = this.state.strtYear
+        
+        let array = this.state.choosenDate.split(["/"])
+
+        let DueDay = Number(array[1])
+        let DueMonth = Number(array[0])   
+        let DueYear = Number(array[2])
+
+        this.setState({ 
+            dueDay: DueDay, 
+            dueMonth: DueMonth, 
+            dueYear: DueYear 
+        })
+
+        //Defining the Term
+        let term = undefined
+        if(strtYear == DueYear && strtMonth == DueMonth){
+            term = "short"
+        }
+        if(strtYear == DueYear && strtMonth != DueMonth){
+            term = "medium"
+        }
+        if(strtYear < DueYear){
+            term = "long"
+        }
+
+        // First Step
+        if(term == 'short'){
+            this.sameMonth(strtDay, strtMonth, strtYear, DueDay, DueMonth, DueYear)
+        }else{
+            if(strtDay <= 15){
+                this.setState({ fortnights: +2 })
+            }else{
+                this.setState({ fortnights: + 1})
+            } 
+        }
+        
+    }
     
+    sameMonth( strtDay, strtMonth, strtYear, dueDay, dueMonth, dueYear ){
+        // February
+        if(dueMonth == 2){
+            if(dueDay < 28){
+                this.setState({ fortnights: +1})
+            }else{
+                this.setState({ fortnights: +2})
+            }
+        }
+        // 30 Days
+        if( dueMonth == 4 || 
+            dueMonth == 6 || 
+            dueMonth == 9 || 
+            dueMonth == 11 
+        ){
+            if(dueDay < 30){
+                this.setState({ fortnights: +1})
+            }else{
+                this.setState({ fortnights: +2})
+            }
+        }
+
+        // 31 Days
+        if( dueMonth == 1 ||
+            dueMonth == 3 ||
+            dueMonth == 5 ||
+            dueMonth == 7 || 
+            dueMonth == 8 || 
+            dueMonth == 10 ||
+            dueMonth == 12 
+        ){
+            if(dueDay < 31){
+                this.setState({ fortnights: +1})
+            }else{
+                this.setState({ fortnights: +2})
+            }
+        }
+    }
 
     render (){
+        const {strtDay, strtMonth, strtYear, reason, amount, fortnights} = this.state
+
         return (
             <View style={styles.container}>
                 <Text style={styles.question}> Saving Plan </Text>
-                <Text style={styles.question}> {this.state.strtDay} </Text>
-                <Text style={styles.question}> {this.state.strtMonth} </Text>
-                <Text style={styles.question}> {this.state.strtYear} </Text>
-                <Text style={styles.question}> {this.state.reason} </Text>
-                <Text style={styles.question}> {this.state.amount} </Text>
-                <Text style={styles.question}> {this.state.choosenDate} </Text>
+                <Text style={styles.question}> {strtDay} </Text>
+                <Text style={styles.question}> {strtMonth} </Text>
+                <Text style={styles.question}> {strtYear} </Text>
+                <Text style={styles.question}> {reason} </Text>
+                <Text style={styles.question}> {amount} </Text>
+                <Text style={styles.question}> {fortnights} </Text>
             </View>
         )
     }
