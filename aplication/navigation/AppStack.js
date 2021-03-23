2ee30bbece4c091c501bf309de23552761e0eb4d
Rafael Anguiano <rj.anguiano.23@gmail.com>
2021-03-22 23:13:53 -0600

    Merging Form and Home

--- aplication/navigation/AppStack.js
+++ aplication/navigation/AppStack.js
@@ -10,13 +10,37 @@ import React from 'react';
 import SavingForm from '../screens/FormScreen'
 import SavingPlan from '../screens/PlanScreen'
 import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import {Button, Text} from 'native-base'
 
 const Stack = createStackNavigator();
  
 export default class AppStack extends React.Component {
   render (){
     return (
-        <Stack.Navigator initialRouteName="Form">
+        <Stack.Navigator initialRouteName="Home">
+          
+          <Stack.Screen 
+            name="Home" 
+            options={({ navigation, route }) => ({
+              headerShown: true, 
+              headerTintColor:'white',
+              headerStyle: { backgroundColor: '#051d5f'},
+              headerRight: () => (
+                <Button
+                  onPress={() => {navigation.navigate('Form')}}
+                  transparent
+                  rounded
+                  style={{backgroundColor:'#051d5f'}}
+                >
+                  <Text style={{fontWeight:'bold', color:'white'}}>+</Text>
+                </Button>
+              )
+            })}
+          >
+            {props => <HomeScreen {...props} />}
+          </Stack.Screen>
+          
           <Stack.Screen name="Form" options={{headerShown: false}}>
             {props => <SavingForm {...props} />}
           </Stack.Screen>
---
+++ aplication/screens/HomeScreen.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { StyleSheet, View} from 'react-native';
+import { Text } from 'native-base'
+
+export default class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            reason: undefined,
+            amount: undefined,
+            choosenDate: undefined,
+            //StrtDate
+            strtDay: undefined,
+            strtMonth: undefined,
+            strtYear: undefined,
+            //DueDate
+            dueDay: undefined,
+            dueMonth: undefined,
+            dueYear: undefined,
+            // Per Fortnight
+            fortnights: 0,
+            biweeklyPay: undefined,
+            calculated: false
+        };
+    }
+
+    render (){    
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}> Home Screen </Text>
+                <Text style={styles.question}> Hello World </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#596997", //#5A7DB0
+      alignItems:'center',
+      justifyContent:'center'
+    },
+    title: {
+        borderRadius: 6,
+        color: "#051d5f",   //#20232a
+        textAlign: "center",
+        fontSize: 30,
+        fontWeight: "bold"
+    },
+    question: {
+        color: "#FFFFFF",   //#20232a
+        textAlign:'center',
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+});
